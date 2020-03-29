f1ef1382023846a5126c21c5a2f505a89dcb7fc7
Julie Braford <jbraford@css.edu>
2020-03-29 16:53:05 -0500

    Finished Assignment 9

--- app/src/main/java/com/example/practice9_studymates/MainActivity.java
+++ app/src/main/java/com/example/practice9_studymates/MainActivity.java
@@ -11,6 +11,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -45,8 +46,9 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        final NavigationView navigationView = findViewById(R.id.nav_view);
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -56,6 +58,52 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+
+                int id = item.getItemId();
+
+                if (id == R.id.nav_add) {
+                    Snackbar.make(getWindow().getDecorView(), "adding study mates is not available yet", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    return true;
+                }
+                if (id == R.id.nav_delete) {
+                    Snackbar.make(getWindow().getDecorView(), "deleting a study mate is not available yet", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    return true;
+                }
+                if (id == R.id.nav_settings) {
+                    startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+                    return true;
+                }
+                if (id == R.id.nav_email) {
+                    Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                    emailIntent.setType("*/*");
+                    emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Hey Study Partner");
+                    if (emailIntent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(emailIntent);
+                    }
+                    return true;
+                }
+                if (id == R.id.nav_sms) {
+                    Intent smsIntent = new Intent(Intent.ACTION_SENDTO);
+                    smsIntent.setData(Uri.parse("sms:2182594411"));
+                    smsIntent.putExtra("sms_body", "Hi");
+                    if (smsIntent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(smsIntent);
+                    }
+                    return true;
+                }
+                return onNavigationItemSelected(item);
+
+
+            }
+
+
+        });
     }
 
     @Override
@@ -65,14 +113,17 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+
+
     public boolean onOptionsItemSelected (MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.action_add) {
+        if (id == R.id.action_add ) {
             Snackbar.make(getWindow().getDecorView(), "adding study mates is not available yet", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show();
             return true;
         }
+
         if (id == R.id.action_delete) {
             Snackbar.make(getWindow().getDecorView(), "deleting a study mate is not available yet", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show();
@@ -105,10 +156,18 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+
+
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+
+
+
+
 }
